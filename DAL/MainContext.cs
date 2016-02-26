84f34edf6c8c9d0068b643c620f5093f437fdfa3
Nick <zinomast@gmail.com>
2016-02-26 10:22:38 +0200

    Skeleton

---
+++ DAL/MainContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+	public class MainContext : DbContext
+	{
+		public MainContext() : base("dbName here")
+		{
+			this.Configuration.LazyLoadingEnabled = true;
+		}
+
+		public MainContext(string connString) : base(connString)
+		{
+			this.Configuration.LazyLoadingEnabled = true;
+		}
+
+		//public DbSet<User> Users { get; set; }
+	}
+}
---
+++ FBAL/Library1.fs
@@ -0,0 +1,4 @@
+﻿namespace FBAL
+
+type Class1() = 
+    member this.X = "F#"
